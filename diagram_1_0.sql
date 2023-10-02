3accb73df384091e4c9bb3246ac706fde567d970
dro0zd <dubr0vin-dk@mail.ru>
2023-10-02 18:54:38 +0300

    -> добавлена первая версия рабочей БД

--- diagram_1_0.sql
+++ diagram_1_0.sql
@@ -1,41 +1,112 @@
 -- Создание таблицы для хранения информации об абитурентах
-CREATE TABLE applicants (
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(255) NOT NULL,
-    address TEXT,
+CREATE TABLE Abiturient (
+    AbiturientID SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	surname VARCHAR(50) NOT NULL,
+    patronymic VARCHAR(50),
+    DateOfBirth DATE NOT NULL,
+    sex VARCHAR(1) NOT NULL,
     phone VARCHAR(20),
-    email VARCHAR(255),
-    photo BYTEA -- Для хранения фото в виде бинарных данных
+    email VARCHAR(100),
+    photo BYTEA
 );
 
--- Создание таблицы для хранения информации о документах абитурентов
-CREATE TABLE applicant_documents (
-    id SERIAL PRIMARY KEY,
-    applicant_id INT REFERENCES applicants(id),
-    document_name VARCHAR(255) NOT NULL,
-    document_file BYTEA, -- Для хранения документов в формате PDF в виде бинарных данных
-    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Создание таблицы для хранения информации о документах (подтверждение личности) абитурентов
+CREATE TABLE Document_identifier (
+    document_identifier_ID SERIAL PRIMARY KEY,
+    Doc_type_ID INT REFERENCES Types_Document_identifier(Doc_type_ID),
+    series_doc INT,
+    number_doc INT,
+    date_issue DATE,
+    org_id INT REFERENCES Issuing_Organization(org_id),
+    presence_of_original BOOLEAN,
+    AbiturientID INT REFERENCES Abiturient(AbiturientID)
 );
 
--- Создание таблицы для расписания и групп
-CREATE TABLE schedule (
-    id SERIAL PRIMARY KEY,
-    group_name VARCHAR(50) NOT NULL,
-    course_name VARCHAR(255),
-    schedule_data JSONB -- Может содержать расписание в формате JSON
+-- Создание таблицы для хранения типов документов (подтверждение личности)
+CREATE TABLE Types_Document_identifier (
+    Doc_type_ID SERIAL PRIMARY KEY,
+    Name_doc_type VARCHAR(255) NOT NULL
 );
 
--- Создание таблицы для информации о преподавателях (если необходимо)
-CREATE TABLE teachers (
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    phone VARCHAR(20)
+-- Создание таблицы для хранения организаций, выдающих документы
+CREATE TABLE Issuing_Organization (
+    org_id SERIAL PRIMARY KEY,
+    code_org INT,
+    Name_org VARCHAR(255) NOT NULL
 );
 
--- Создание таблицы для доступа преподавателей к расписанию
-CREATE TABLE teacher_access (
-    id SERIAL PRIMARY KEY,
-    teacher_id INT REFERENCES teachers(id),
-    schedule_id INT REFERENCES schedule(id)
+-- Создание таблицы для хранения информации об образовании абитурентов
+CREATE TABLE Document_education (
+    document_identifier_ID SERIAL PRIMARY KEY,
+    Doc_type_ID INT REFERENCES Types_Document_education(Doc_type_ID),
+    series_doc INT,
+    number_doc INT,
+    date_issue DATE,
+    org_id INT REFERENCES Education_Organization(org_id),
+    AbiturientID INT REFERENCES Abiturient(AbiturientID)
 );
+
+-- Создание таблицы для хранения типов образовательных документов
+CREATE TABLE Types_Document_education (
+    Doc_type_ID SERIAL PRIMARY KEY,
+    Name_doc_type VARCHAR(255) NOT NULL
+);
+
+-- Создание таблицы для хранения информации об образовательных организациях
+CREATE TABLE Education_Organization (
+    org_id SERIAL PRIMARY KEY,
+    Name_org VARCHAR(255) NOT NULL
+);
+
+-- Создание таблицы для хранения заявлений на поступление
+CREATE TABLE Application_for_admission (
+    ApplicationID SERIAL PRIMARY KEY,
+    AbiturientID INT REFERENCES Abiturient(AbiturientID),
+    SpecialityID INT REFERENCES Specialty_for_study(SpecialityID),
+    data_application DATE,
+    EmployeeID INT REFERENCES Employee(EmployeeID)
+);
+
+-- Создание таблицы для хранения специальностей для обучения
+CREATE TABLE Specialty_for_study (
+    SpecialityID SERIAL PRIMARY KEY,
+    code_speciality VARCHAR(20) NOT NULL,
+    name_speciality VARCHAR(50) NOT NULL,
+    amount_place INT
+);
+
+-- Создание таблицы для связи специальностей и экзаменов
+CREATE TABLE Programm_for_study (
+    SpecialityID INT REFERENCES Specialty_for_study(SpecialityID),
+    ExamID INT REFERENCES Exam(ExamID)
+);
+
+-- Создание таблицы для хранения информации об экзаменах
+CREATE TABLE Exam (
+    ExamID SERIAL PRIMARY KEY,
+    name_exam VARCHAR(50) NOT NULL
+);
+
+-- Создание таблицы для хранения результатов экзаменов
+CREATE TABLE Exam_res (
+    AbiturientID INT REFERENCES Abiturient(AbiturientID),
+    ExamID INT REFERENCES Exam(ExamID),
+    score INT
+);
+
+-- Создание таблицы для хранения информации о сотрудниках
+CREATE TABLE Employee (
+    EmployeeID SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+	surname VARCHAR(50) NOT NULL,
+    patronymic VARCHAR(50),
+    PositionID INT REFERENCES Positions_rights(PositionID)
+);
+
+-- Создание таблицы для хранения прав сотрудников
+CREATE TABLE Positions_rights (
+    PositionID SERIAL PRIMARY KEY,
+    name_position VARCHAR(50) NOT NULL,
+    rights_position VARCHAR(50) NOT NULL
+)
